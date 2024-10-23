0516da5640569d2d06e41c3b8bd844f72ddbb38b
Prachi123G <prachighate02@gmail.com>
2024-10-23 16:57:16 +0530

    Multiplication

--- multiplication.java
+++ multiplication.java
@@ -2,11 +2,13 @@ public class multiplication {
     public static void main(String[] args){
         int a=3;
         int b=1;
-        int c=a-b;
+        int c=a
+                * b;
         System.out.println(c);
 
 
-// addition
+
+
 
 
 
